f0cab6652f9b69331acb925f0b60997a079380e9
Vít Šefl <vituscze@gmail.com>
2014-06-25 00:55:38 +0200

    Change contexts a bit.

--- src/Compiler/TypeChecking/Context.hs
+++ src/Compiler/TypeChecking/Context.hs
@@ -5,15 +5,13 @@ module Compiler.TypeChecking.Context
     , KindCtx
 
     -- * Types.
-    , Scheme
+    , Scheme(..)
     , TyCtx
     )
     where
 
 import qualified Data.Map as Map
 import Data.Map (Map)
-import qualified Data.Set as Set
-import Data.Set (Set)
 
 import Compiler.AST
 
@@ -24,14 +22,10 @@ import Compiler.AST
 type Kind = Int
 
 -- | Kind context.
+--
+--   Used for kind checking of data definitions and user-declared types.
+--   Internal types have correct kind by construction.
 type KindCtx = Map Name Kind
-
--- | Type schemes for algorithm W.
-data Scheme
-    = Scheme
-        Type       -- ^ Actual type.
-        (Set Name) -- ^ Quantified variables.
-    deriving (Show)
 
 -- | Typing context.
 type TyCtx = Map Name Scheme
