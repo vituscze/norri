47f82fca32c673748f183202857a479d11278cfa
Vít Šefl <vituscze@gmail.com>
2014-06-26 14:23:21 +0200

    Use 'Applicative' instead of 'liftM' and friends.

--- src/Compiler/Transform.hs
+++ src/Compiler/Transform.hs
@@ -13,6 +13,7 @@ module Compiler.Transform
     )
     where
 
+import Control.Applicative
 import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
@@ -48,8 +49,6 @@ free (Let decls e) = (free e \\ names) `Set.union` vars
 --
 --   A definition @ValueDef n e@ is recursive, if @n@ is a free variable
 --   inside the expression @e@.
---
---   Note that @fix@ should not be bound in a @Let@ or @Lam@.
 fixify :: ValueDef -> ValueDef
 fixify v@(ValueDef n e)
   | n `Set.member` vars = ValueDef n (Fix n (fixify' e))
@@ -91,17 +90,17 @@ rename :: Int  -- ^ Starting number.
 rename s e = evalState (runReaderT (go e) Map.empty) s
   where
     -- Apply the renaming inside expression.
-    go (Var v)       = liftM (Var . fromMaybe v) (asks (Map.lookup v))
-    go (Lam x e)     = liftM (uncurry Lam) (localInsert x (go e))
-    go (App e1 e2)   = liftM2 App (go e1) (go e2)
-    go (SetType e t) = liftM (\e -> SetType e t) (go e)
+    go (Var v)       = Var . fromMaybe v <$> asks (Map.lookup v)
+    go (Lam x e)     = uncurry Lam <$> localInsert x (go e)
+    go (App e1 e2)   = App <$> go e1 <*> go e2
+    go (SetType e t) = flip SetType t <$> go e
     go (NumLit n)    = return (NumLit n)
-    go (Fix x e)     = liftM (uncurry Fix) (localInsert x (go e))
+    go (Fix x e)     = uncurry Fix <$> localInsert x (go e)
     go (Let decls e) = go' decls
       where
-        go' []                = liftM (Let []) (go e)
+        go' []                = Let [] <$> go e
         go' (ValueDef n e:ds) = do
-            let renamed = liftM2 (,) (go e) (go' ds)
+            let renamed = (,) <$> go e <*> go' ds
             (n', (e', Let decls' outer)) <- localInsert n renamed
             return (Let (ValueDef n' e':decls') outer)
 
