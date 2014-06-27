97cbb9efe8dcca56b03bbd9c32740715dd84b902
Vít Šefl <vituscze@gmail.com>
2014-06-27 20:18:21 +0200

    Temporary Main module.

---
+++ src/Main.hs
@@ -0,0 +1,38 @@
+-- Temporary main.
+module Main
+    (
+      main
+    )
+    where
+
+import qualified Data.Map as Map
+import Data.Map (Map)
+import System.Exit
+import System.IO
+import Text.Parsec
+
+import Compiler.Parser
+import Compiler.Pretty
+import Compiler.Transform
+import Compiler.TypeChecking.Context
+import Compiler.TypeChecking.Infer
+
+defaultCtx :: TICtx
+defaultCtx =
+  ( Map.fromList [("Int", 0)]
+  , Map.fromList []
+  , Map.fromList []
+  )
+
+main :: IO ()
+main = do
+    input <- readFile "input.tmpc"
+    ast <- case parse file "" input of
+        Right ast -> return ast
+        Left  err -> putStrLn "Parsing error:" >> print err >> exitFailure
+    let ast' = freshModule (fixifyModule ast)
+    case runTI (inferModule defaultCtx ast') of
+        Right _   -> return ()
+        Left  err -> putStrLn "Type checking error:" >> print err >> exitFailure
+    writeFile "output.cpp" (prettyModule ast')
+
