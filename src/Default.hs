7a68bd5b4e4591943f8a6e00272a6e44682c6c1f
Vít Šefl <vituscze@gmail.com>
2014-07-02 19:24:18 +0200

    Move the default values into separate module.

---
+++ src/Default.hs
@@ -0,0 +1,76 @@
+-- | Default values for file names, contexts, etc.
+module Default
+    (
+    -- * Default contexts
+      defaultErrCtx
+    , defaultTICtx
+
+    -- * Module inference
+    , runDefault
+
+    -- * Default file names
+    , defaultOutput
+    )
+    where
+
+import qualified Data.Map as Map
+import Text.Parsec
+
+import Compiler.AST
+import Compiler.Parser
+import Compiler.TypeChecking.Context
+import Compiler.TypeChecking.Error
+import Compiler.TypeChecking.Infer
+
+-- | Default error context.
+defaultErrCtx :: ErrCtx
+defaultErrCtx = []
+
+-- | Default type inference context.
+defaultTICtx :: TICtx
+defaultTICtx = TICtx
+    -- Type constructors.
+    (Map.fromList
+        [ ("Bool", 0)
+        , ("Int",  0)
+        , ("Type", 0)
+        ])
+
+    -- Values.
+    (Map.fromList
+        [ ("fix",   ty "(a -> a) -> a")
+        , ("neg",   ty "Int -> Int")
+        , ("plus",  ty "Int -> Int -> Int")
+        , ("minus", ty "Int -> Int -> Int")
+        , ("mul",   ty "Int -> Int -> Int")
+        , ("div",   ty "Int -> Int -> Int")
+        , ("rem",   ty "Int -> Int -> Int")
+        , ("eq",    ty "Int -> Int -> Bool")
+        , ("neq",   ty "Int -> Int -> Bool")
+        , ("lt",    ty "Int -> Int -> Bool")
+        , ("le",    ty "Int -> Int -> Bool")
+        , ("gt",    ty "Int -> Int -> Bool")
+        , ("ge",    ty "Int -> Int -> Bool")
+        , ("not_",  ty "Bool -> Bool")
+        , ("and_",  ty "Bool -> Bool -> Bool")
+        , ("or_",   ty "Bool -> Bool -> Bool")
+        , ("xor_",  ty "Bool -> Bool -> Bool")
+        , ("if_",   ty "Bool -> a -> a -> a")
+        ])
+
+    -- Type signatures.
+    (Map.fromList [])
+  where
+    ty t = case parse scheme "" t of
+        Right ts -> ts
+        Left  _  -> error "Fatal error: failed to parse the type of\
+                          \ built-in function."
+
+-- | Run the type inference for a given module with default contexts
+--   and return the result.
+runDefault :: Module -> Either TCError TICtx
+runDefault ast = runTI (inferModule ast) defaultErrCtx defaultTICtx
+
+-- | Default output file for the resulting C++ code.
+defaultOutput :: String
+defaultOutput = "a.cpp"
--- src/Main.hs
+++ src/Main.hs
@@ -5,52 +5,16 @@ module Main
     )
     where
 
-import qualified Data.Map as Map
 import Data.Maybe
 import Options.Applicative
-import System.Exit
 import Text.Parsec
 
 import Compiler.Compile
 import Compiler.Parser
 import Compiler.Transform
-import Compiler.TypeChecking.Context
-import Compiler.TypeChecking.Infer
+import Default
 import Options
 import Report
-
-defaultCtx :: TICtx
-defaultCtx = TICtx
-    (Map.fromList [("Bool", 0), ("Int", 0), ("Type", 0)])
-    (Map.fromList
-        [ ("fix",   ty "(a -> a) -> a")
-        , ("neg",   ty "Int -> Int")
-        , ("plus",  ty "Int -> Int -> Int")
-        , ("minus", ty "Int -> Int -> Int")
-        , ("mul",   ty "Int -> Int -> Int")
-        , ("div",   ty "Int -> Int -> Int")
-        , ("rem",   ty "Int -> Int -> Int")
-        , ("eq",    ty "Int -> Int -> Bool")
-        , ("neq",   ty "Int -> Int -> Bool")
-        , ("lt",    ty "Int -> Int -> Bool")
-        , ("le",    ty "Int -> Int -> Bool")
-        , ("gt",    ty "Int -> Int -> Bool")
-        , ("ge",    ty "Int -> Int -> Bool")
-        , ("not_",  ty "Bool -> Bool")
-        , ("and_",  ty "Bool -> Bool -> Bool")
-        , ("or_",   ty "Bool -> Bool -> Bool")
-        , ("xor_",  ty "Bool -> Bool -> Bool")
-        , ("if_",   ty "Bool -> a -> a -> a")
-        ])
-    (Map.fromList [])
-  where
-    ty t = case parse scheme "" t of
-        Right ts -> ts
-        Left  _  -> error "Fatal error: failed to parse the type of\
-                          \ built-in function."
-
-defaultOutput :: String
-defaultOutput = "a.cpp"
 
 main :: IO ()
 main = do
@@ -60,11 +24,9 @@ main = do
         output = fromMaybe defaultOutput (outputFile opts)
 
     code <- readFile input
-    ast  <- case parse file "" code of
-        Right ast -> return ast
-        Left  err -> print err >> exitFailure
+    ast  <- either reportParseError return (parse file input code)
+
     let ast' = fixifyModule ast
-    case runTI (inferModule ast') [] defaultCtx of
-        Right _   -> return ()
-        Left  err -> reportTCError err
+
+    _    <- either reportTCError return (runDefault ast')
     writeFile output (compileModule (freshModule ast'))
