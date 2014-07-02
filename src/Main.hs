dbe2d900a15063b825cc01c48a8c053e132a6362
Vít Šefl <vituscze@gmail.com>
2014-07-02 22:22:21 +0200

    New main.

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,24 @@
--- Temporary main.
+-- The main module.
 module Main
     (
+    -- * Main
       main
+
+    -- * Compilation
+    , compile
     )
     where
 
+import Control.Monad
+import Data.Char
+import Data.List
 import Data.Maybe
 import Options.Applicative
 import Text.Parsec
+import System.FilePath
+import System.IO
 
+import Compiler.AST
 import Compiler.Compile
 import Compiler.Parser
 import Compiler.Transform
@@ -16,12 +26,42 @@ import Default
 import Options
 import Report
 
+-- | Compile the module and write it into specified file.
+compile :: Module
+        -> Bool      -- ^ Whether to add the content directly.
+        -> Bool      -- ^ Whether to add @#include@ guards.
+        -> FilePath  -- ^ Include directory.
+        -> FilePath  -- ^ Destination.
+        -> IO ()
+compile m direct guards include dest = withFile dest WriteMode $ \h -> do
+    let guardName = "TMPCOMPILER_OUTPUT_" ++ map toUpper (takeBaseName dest)
+    when guards $ do
+        hPutStrLn h $ "#ifndef " ++ guardName
+        hPutStrLn h $ "#define " ++ guardName
+    addHeader h
+    hPutStrLn h $ compileModule (freshModule m)
+    when guards $ do
+        hPutStrLn h $ "#endif // " ++ guardName
+  where
+    addHeader h = forM_ defaultRuntime $ \rt ->
+        let path = include </> rt
+        in if direct
+            then readFile path >>= hPutStrLn h
+            else hPutStrLn h . concat $
+                [ "#include \""
+                , intercalate "/" $ splitDirectories path
+                , "\""
+                ]
+
 main :: IO ()
 main = do
     opts <- execParser options
 
-    let input  = inputFile opts
-        output = fromMaybe defaultOutput (outputFile opts)
+    let input   = inputFile opts
+        output  = fromMaybe defaultOutput (outputFile opts)
+        direct  = addRuntime opts
+        include = fromMaybe defaultInclude (includeDir opts)
+        guards  = useGuards opts
 
     code <- readFile input
     ast  <- either reportParseError return (parse file input code)
@@ -29,4 +69,4 @@ main = do
     let ast' = fixifyModule ast
 
     _    <- either reportTCError return (runDefault ast')
-    writeFile output (compileModule (freshModule ast'))
+    compile ast' direct guards include output
